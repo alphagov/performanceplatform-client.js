eef0452bc5df96b44e9d076b4a5c9641d63873e2
Ben Marvell <madmarv187@gmail.com>
2014-11-28 17:05:33 +0000

    We now have a getDashboardMetrics method that returns a dashboard with resolved modules.
    Changes the way the Dashboard constructor works. We now pass a dashboard slug in a init.

--- test/dashboard.spec.js
+++ test/dashboard.spec.js
@@ -14,16 +14,28 @@ describe('Dashboard', function () {
     Dashboard = requireSubvert.require('../lib/dashboard');
   });
 
+  describe('init', function () {
+    it('sets the dashboard slug and query options', function () {
+      var dashboard = new Dashboard('test-slug');
+
+      dashboard.options.should.eql({
+        json: true,
+        useQuerystring: true,
+        slug: 'test-slug'
+      });
+    });
+  });
+
   describe('getConfig()', function () {
 
     it('Should respond with a dashboard', function () {
 
-      var dashboard = new Dashboard();
       var testSlug = 'test-dashboard-slug';
+      var dashboard = new Dashboard(testSlug);
 
       deferred.resolve(dashboardResponse);
 
-      return dashboard.getConfig(testSlug)
+      return dashboard.getConfig()
         .then(function (dashboardConfig) {
 
           stub.should.be.calledOnce;
@@ -31,13 +43,48 @@ describe('Dashboard', function () {
             .should.eql({
               url:
                 'https://stagecraft.production.performance.service.gov.uk/public/dashboards?slug=' +
-                  testSlug
+                  testSlug,
+              json: true,
+              useQuerystring: true,
+              slug: 'test-dashboard-slug'
             });
 
           dashboardConfig.should.equal(dashboardResponse);
         });
     });
 
+  });
+
+  describe('getKPI()', function () {
+    it('should respond with a resolved KPI modules data', function () {
+      var module = {
+        'module-type': 'kpi',
+        'data-source': {
+          'data-group': 'transactional-services',
+          'data-type': 'summaries',
+          'query-params': {
+            'sort_by': '_timestamp:descending',
+            'filter_by': [
+              'service_id:bis-accounts-filing',
+              'type:seasonally-adjusted'
+            ]
+          }
+        }
+      };
+
+      var moduleDataResponse = [{}, {}, {}];
+
+      var dashboard = new Dashboard('test-dashboard');
+
+      deferred.resolve({
+        data: moduleDataResponse
+      });
+
+      return dashboard.getKPI(module)
+        .then(function (kpiData) {
+          kpiData.data.should.equal(moduleDataResponse);
+        });
+    });
   });
 
 });
